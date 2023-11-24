8b03082acd2a778bc6de1f85c3d0189f3a402da1
uuphoria <123761856+uuphoria@users.noreply.github.com>
2023-11-24 11:50:59 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <title>~ u u p h o r i a ~</title>
-    <div class="center">
-    <img class="uuphoria" src="./resources/apple.png" alt="uuphoria"/><br />
-    <link rel="stylesheet" href="template.css"/>
-    <script>
-        history.pushState("https://uuphoria.github.io","~ u u p h or i a ~","/"); // in case somebody got here with a ?redirect
-    </script>
-</head>
-<body>
-    <div class="center">
-        <p>
-            <i>hi my name is uuphoria gmanthemarioguy youtube wise add /projects if theres already / just add projects to check out my project stuff :P</i> <br />
-            <br />
-            my main interests in programming or computers are sm64js, js ubuntu<br/>
-            <br />
-            idk what lese to say i guess <br />
-        </p>
+<?php
+echo '<!DOCTYPE html>';
+echo '<html lang="en">';
+echo '';
+echo '<head>';
+echo '<meta charset="UTF-8" />';
+echo '<title>~ u u p h o r i a ~</title>';
+echo '<div class="center">';
+echo '<img class="uuphoria" src="./resources/apple.png" alt="uuphoria"/><br />';
+echo '<link rel="stylesheet" href="template.css"/>';
+echo '<script>';
+echo 'history.pushState("https://uuphoria.github.io","~ u u p h or i a ~","/"); // in case somebody got here with a ?redirect';
+echo '</script>';
+echo '</head>';
+echo '<body>';
+echo '<div class="center">';
+echo '<p>';
+echo '<i>hi my name is uuphoria gmanthemarioguy youtube wise add /projects if theres already / just add projects to check out my project stuff :P</i> <br />';
+echo '<br />';
+echo 'my main interests in programming or computers are sm64js, js ubuntu<br/>';
+echo '<br />';
+echo 'idk what lese to say i guess <br />';
+echo '</p>';
+echo '';
+?>
